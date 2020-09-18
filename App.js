5da2769b813a50cefba5ffaefa640531177bd20c
Carlos.Inhani <carlos.inhani@gmail.com>
2020-09-17 21:20:18 -0300

    Calculate_Button

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  calcularIMC = () => {
+   const peso = 7;
+   const altura = 2;
+
+    const imc = Math.pow(peso, altura);
+
+    alert(imc);
+  }
+  
+  render() { 
+    const imc = 25;
+    const legenda = 'Normal';
+
   return (
-    <View style={styles.container}>
-      <Text>Teste</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+     <View style={styles.app}>
+        <Text style={styles.legenda}>Seu IMC</Text>  
+
+        <View>
+          <Text style={styles.resultado}>{imc}</Text>
+          <Text style={styles.diagnostico}>{legenda}</Text>
+        </View>
+
+        <View>
+          <TextInput style={styles.peso} />
+          <TextInput style={styles.altura} />
+          <Button title="Calcular" onPress={this.calcularIMC} />
+        </View>
+     </View>
+   );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  app: {
+    padding: 10,
   },
+  legenda: {
+    textAlign: 'center',
+    fontWeight: "bold",
+  },
+  resultado:{
+     textAlign: 'center',
+     fontSize: 22,
+     fontWeight: 'bold',   
+  },
+  diagnostico: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  peso: {
+     borderColor: '#000',
+     borderWidth: 1,
+  },  
+  altura: {
+    borderColor: '#000',
+     borderWidth: 1, 
+  },
+  
 });
